539fcd27304e2edb0affb57fe5c99758f7e37e14
Ondřej Krsička <ondrej.krsicka@gmail.com>
2025-06-08 12:26:15 +0200

    Copied https://github.com/krsion/samples/tree/main/orleans/BankAccount here

---
+++ BankServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection; // Add this to fix CS1061
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans.Transactions.Abstractions; // Add this to fix CS0246
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddConsole(); // Enable console logging
+        logging.SetMinimumLevel(LogLevel.Debug); // Set minimum log level
+    })
+    .UseOrleans(siloBuilder =>
+    {
+        siloBuilder
+            .UseLocalhostClustering()
+            .AddMemoryGrainStorageAsDefault()
+            .UseTransactions();
+    })
+    .RunConsoleAsync();
